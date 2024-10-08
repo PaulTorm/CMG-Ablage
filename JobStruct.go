7ae456d32a1f0a9405f213a687f15f7b74e0f817
PaulTorm <tormaehlen@denic.de>
2024-10-08 18:49:59 +0200

    Change execargs to slices and add env vars

--- JobStruct.go
+++ JobStruct.go
@@ -4,7 +4,8 @@ type Job struct {
 	id               int
 	status           int // e.g 0 = new job, 1 = scheduled job, 2 = currently in work, 3 = finished
 	image            string
-	execution_args   string
+	execution_args   []string
+	environment_vars []string
 	logs_path        string
 	artifacts_path   string
 	scheduled_worker string
