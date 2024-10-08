29a7a0c02c14a44fec8fd47405f792b839221c4d
PaulTorm <tormaehlen@denic.de>
2024-10-08 15:01:54 +0200

    Draft of JobStruct

---
+++ JobStruct.go
@@ -0,0 +1,12 @@
+package main
+
+type Job struct {
+	id               int
+	status           int // e.g 0 = new job, 1 = scheduled job, 2 = currently in work, 3 = finished
+	image            string
+	execution_args   string
+	logs_path        string
+	artifacts_path   string
+	scheduled_worker string
+	exitcode         int
+}
